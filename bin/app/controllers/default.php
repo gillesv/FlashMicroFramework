f7803686c80bcc1ede6ea12531631cedc3f13c02
Gilles Vandenoostende <gilles.vandenoostende@gmail.com>
2011-10-27 16:25:07 +0200

    second attempt

--- bin/app/controllers/default.php
+++ bin/app/controllers/default.php
@@ -4,8 +4,10 @@ function index() {
   return html('index.html.php', 'layout.html.php');
 }
 
+function pages() {
+  return html(params('page') . '.html.php', 'layout.html.php');
+}
+
 function index_catchall() {
   return html('index.html.php', 'layout.html.php');
 }
-
-?>
--- bin/app/lib/proximitybbdo/apploader.php
+++ bin/app/lib/proximitybbdo/apploader.php
@@ -13,36 +13,31 @@
  */
 # ============================================================================ #
 
+// Require spyc for YAML parsing.
 require_once(dirname(dirname(__FILE__)) . "/spyc/spyc.php");
 
 class ProximityApp {
   public static $settings = array();
+  public static $settings_file = 'config.yml';
 
-  /**
-   * Parse a yaml config file and save it in an array
-   */
+  // Parse a yaml config file and save it in an array
   private static function load_settings($settings_file) {
     self::$settings = spyc_load_file($settings_file);
   }
 
-  /**
-   * Inits the actual app, putting together a skeleton of
-   * niceness to use in your app, like multilang
-   */
-  public static function init($settings_file) {
-    // step one, parse settings
-    self::load_settings($settings_file);
-    
-    // init Multilang
-    
+  // Inits the actual app, putting together a skeleton of
+  // niceness to use in your app, like multilang
+  public static function init($config_directory, $param_settings_file = '') {
+    // Step one, parse settings
+    self::load_settings($config_directory . (strlen($param_settings_file) > 0 ? $param_settings_file : self::$settings_file));
+
+    // Init Multilang
     Multilang::getInstance()->init();
   }
 }
 
-/**
- * Helper _c function defined outside of the apploader class
- * for easy of use
- **/
+// Helper _c function defined outside of the apploader class
+// for easy of use
 function _c() {
   $result = ProximityApp::$settings;
 
