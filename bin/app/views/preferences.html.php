288ba7abe5e51e662c628206825ae26c642dfe3d
Gilles Vandenoostende <gilles.vandenoostende@gmail.com>
2011-11-04 15:54:16 +0100

    preferences page

--- bin/app/views/preferences.html.php
+++ bin/app/views/preferences.html.php
@@ -1,16 +1,18 @@
-<form action="<?php echo(url_for('/preferences/save')); ?>" id="splash" method="post">
+<form action="<?php echo(url_for('/preferences/save')); ?>" id="preferences" method="post">
 	<h1>This website is designed to be an immersive experience</h1>
 	
 	<p>The best experience requires the use of the Flash player plugin. However, all content is perfectly accessible with just a modern HTML5-compliant browser.</p>
 	
 	<h3>Make your choice</h3>
 	
-	<div class="checkboxes">
-		<ul>
-			<li><input type="radio" id="splash_choice_yes" name="choice" value="yes" checked /> 	<label for="splash_choice_yes">Use Flash</label></li>
-			<li><input type="radio" id="splash_choice_no" name="choice" value="no" /> 	<label for="splash_choice_no">Just give me the content</label></li>
-		</ul>
-	</div>
-	
-	<input type="submit" value="Submit" />
+	<fieldset>
+		<div class="checkboxes">
+			<ul>
+				<li><input type="radio" id="splash_choice_yes" name="choice" value="yes" checked /> 	<label for="splash_choice_yes">Use Flash</label></li>
+				<li><input type="radio" id="splash_choice_no" name="choice" value="no" /> 	<label for="splash_choice_no">Just give me the content</label></li>
+			</ul>
+		</div>
+		
+		<input type="submit" value="Submit" />
+	</fieldset>
 </form>
